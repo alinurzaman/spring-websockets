7ffb9b8785cc62097647ee2caf8f8868e91820af
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-07-12 07:16:37 +0700

    first implementation

---
+++ src/main/java/com/example/springwebsockets/Greeting.java
@@ -0,0 +1,12 @@
+package com.example.springwebsockets;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+    private String message;
+}
